baba7106ad0058e07610c973443a495a65c5abdc
Philip Edekobi <philipedekobi@gmail.com>
2022-08-15 20:55:31 +0100

    main functionality completed

--- test/help.js
+++ test/help.js
@@ -1,5 +1,18 @@
 "use strict";
 
 const assert = require("assert");
-const fs = require("fs");
-const spawn = require("child_process").spawn;
+const spawnSync = require("child_process").spawnSync;
+
+describe("help", () => {
+  describe("cli", () => {
+    it("prints a general help message", () => {
+      const out = spawnSync("node", ["./bin/pets-cli", "help"], {
+        cwd: __dirname + "/..",
+      });
+
+      assert.ok(/Usage: pets/.test(out.stdout));
+      assert.ok(/help/.test(out.stdout));
+      assert.ok(/init/.test(out.stdout));
+    });
+  });
+});
--- test/init.js
+++ test/init.js
@@ -49,7 +49,35 @@ describe("init", () => {
           }
         })();
       } catch (error) {
-        console.log(error);
+        throw new Error(error);
+      }
+    });
+
+    it("creates db successfully with specified directory", (done) => {
+      try {
+        let buffer = "";
+
+        (async () => {
+          const child = spawn("node", ["./bin/pets-cli", "init", "./test"], {
+            cwd: __dirname + "/..",
+          });
+
+          child.stdout.on("data", (buffString) => {
+            buffer += buffString.toString();
+          });
+
+          child.on("close", () => {
+            assert.equal(buffer, "DB created and initialized successfully\n");
+            done();
+            cleanup();
+          });
+
+          function cleanup() {
+            fs.unlinkSync("./test/db.json");
+          }
+        })();
+      } catch (error) {
+        throw new Error(error);
       }
     });
   });
@@ -57,6 +85,82 @@ describe("init", () => {
   describe("api", () => {
     it("returns an error on invalid directory", async () => {
       return assert.rejects(async () => await init("ppp"));
+    });
+
+    it("creates db successfully at specified path", async () => {
+      const testPath = `${__dirname}/placeholder`;
+
+      try {
+        await init(testPath);
+        if (fs.existsSync(`${testPath}/db.json`)) return assert.ok(true);
+        else throw new Error("create db failed");
+      } catch (error) {
+        throw new Error(error);
+      } finally {
+        fs.unlinkSync(`${testPath}/db.json`);
+      }
+    });
+
+    it("works when output filename is provided", async () => {
+      const testPath = `${__dirname}/..`;
+
+      try {
+        await init(testPath, "test");
+        if (fs.existsSync(`${testPath}/test.json`)) return assert.ok(true);
+        else throw new Error("create db failed");
+      } catch (error) {
+        throw new Error(error);
+      } finally {
+        fs.unlinkSync(`${testPath}/test.json`);
+      }
+    });
+
+    it("works when number of rows is provided", async () => {
+      const testPath = `${__dirname}/..`;
+
+      try {
+        await init(testPath, null, 210);
+
+        if (fs.existsSync(`${testPath}/db.json`)) {
+          const strJson = fs.readFileSync(`${testPath}/db.json`, {
+            encoding: "utf-8",
+          });
+
+          const json = JSON.parse(strJson);
+
+          assert.equal(json.pets.length, 210);
+        } else {
+          throw new Error("create db failed");
+        }
+      } catch (error) {
+        throw new Error(error);
+      } finally {
+        fs.unlinkSync(`${testPath}/db.json`);
+      }
+    });
+
+    it("works when all 3 arguments are passed", async () => {
+      const testPath = `${__dirname}/..`;
+
+      try {
+        await init(testPath, "test", 240);
+
+        if (fs.existsSync(`${testPath}/test.json`)) {
+          const strJson = fs.readFileSync(`${testPath}/test.json`, {
+            encoding: "utf-8",
+          });
+
+          const json = JSON.parse(strJson);
+
+          assert.equal(json.pets.length, 240);
+        } else {
+          throw new Error("create db failed");
+        }
+      } catch (error) {
+        throw error;
+      } finally {
+        fs.unlinkSync(`${testPath}/test.json`);
+      }
     });
   });
 });
